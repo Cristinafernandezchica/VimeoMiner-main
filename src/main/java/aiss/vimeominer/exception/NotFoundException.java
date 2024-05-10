af489bb8f92788dbb23c821e322959d7d5f05255
AngelAmoFP <angamosan@alum.us.es>
2024-05-10 20:26:58 +0200

    Exception edited

--- src/main/java/aiss/vimeominer/exception/NotFoundException.java
+++ src/main/java/aiss/vimeominer/exception/NotFoundException.java
@@ -4,7 +4,7 @@ package aiss.vimeominer.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code= HttpStatus.NOT_FOUND, reason= "not find")
+@ResponseStatus(code= HttpStatus.NOT_FOUND, reason= "Resource not found")
 
 public class NotFoundException extends Exception {
 }
